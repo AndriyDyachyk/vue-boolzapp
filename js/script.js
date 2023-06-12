b12e82eb83ca39f7aa02f12c4f68028bb2f4809f
Andriy Dyachyk <andriy.dyachyk98@gmail.com>
2023-06-12 12:17:10 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('principi.js loaded');
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+}).mount('#app')
+
+
